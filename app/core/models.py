45d2efc9fd5339622da8f29da5cf07d1f31ecd87
nts2pd <nts2pd@gmail.com>
2020-08-08 15:07:49 -0400

    fix line spacing

--- app/core/models.py
+++ app/core/models.py
@@ -15,7 +15,6 @@ class UserManager(BaseUserManager):
 
         return user
 
-
     def create_superuser(self, email, password):
         """creates and saves new superuser"""
         user = self.create_user(email, password)
@@ -36,5 +35,3 @@ class User(AbstractBaseUser, PermissionsMixin):
     objects = UserManager()
 
     USERNAME_FIELD = 'email'
-
-
--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -16,7 +16,6 @@ class ModelTests(TestCase):
         self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
 
-
     def test_new_user_email_normalized(self):
         """test the email for new user is normalized"""
         email = 'fake@fakeemail.com'
@@ -24,12 +23,10 @@ class ModelTests(TestCase):
 
         self.assertEqual(user.email, email.lower())
 
-
     def test_new_user_invalid_user(self):
         """test creating user with no email raises error"""
         with self.assertRaises(ValueError):
             get_user_model().objects.create_user(None, '123123')
-
 
     def test_create_new_superuser(self):
         """test creating new superuser"""
